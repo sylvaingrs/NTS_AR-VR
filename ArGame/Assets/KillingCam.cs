f8b5cf8198f00251ac786cda1c9a5323eb78808e
lucas stephan <lucas.stephan@epita.fr>
2023-03-29 13:02:49 +0200

    end 1st session

---
+++ ArGame/Assets/KillingCam.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingCam : MonoBehaviour
+{
+    public GameObject particleEffect;
+    public Vector2 touchpos;
+    private RaycastHit hit;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Test AR LOG");
+        if (Input.touchCount <= 0)
+            return;
+
+        touchpos = Input.GetTouch(0).position;
+
+        Ray ray = cam.ScreenPointToRay(touchpos);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObj = hit.collider.gameObject;
+            if (hitObj.tag == "Enemy")
+            {
+                var clone = Instantiate(particleEffect, hitObj.transform.position, Quaternion.identity);
+                clone.transform.localScale = hitObj.transform.localScale;
+                Destroy(hitObj);
+            }
+        }
+    }
+}
